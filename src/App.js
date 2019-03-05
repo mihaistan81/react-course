8eace2952affd6e876c26843a7a8e4f3ffcdf585
mihai stan <mihai.stan@gmail.com>
2019-03-06 00:00:08 +0200

    2 way binding

--- src/App.js
+++ src/App.js
@@ -13,12 +13,22 @@ class App extends Component {
     otherProp: "Some other Property"
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('Was clicked');
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28},
+        { name: newName, age: 28},
         { name: 'Alina', age: 29},
+        { name: 'Stephanie', age: 30}
+      ]
+    });
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28},
+        { name: event.target.value, age: 29},
         { name: 'Stephanie', age: 30}
       ]
     });
@@ -33,9 +43,14 @@ class App extends Component {
 
         <div className="App-body" >
 
-            <button onClick={this.switchNameHandler}>Switch name</button>
+            <button onClick={() => this.switchNameHandler('Maxy !!')}>Switch name</button>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler}>I am a romanian!</Person>
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age} 
+              click={this.switchNameHandler.bind(this, 'Maximilitian')}
+              changed={this.onChangeHandler.bind(this)}
+              >I am a romanian!</Person>
             <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
             <div>{this.state.otherProp}</div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <p onClick={props.click}>I am {props.name} and I am {props.age} years old!
-      {props.children}
-    </p>
+    <div className="Person">
+      <p onClick={props.click}>I am {props.name} and I am {props.age} years old!
+        {props.children}
+      </p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+    
   );
 };
 
